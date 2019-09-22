e65da86602b6222e47aba672943a5ec15cf50e79
landen943 <37804362+landen943@users.noreply.github.com>
2019-09-22 16:20:14 -0500

    stuff

--- src/main/java/landospuk/maggotcheesemod/client/renders/MaggotEntityRender.java
+++ src/main/java/landospuk/maggotcheesemod/client/renders/MaggotEntityRender.java
@@ -5,14 +5,14 @@ import landospuk.maggotcheesemod.client.models.MaggotEntityModel;
 import landospuk.maggotcheesemod.entities.MaggotEntity;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.LivingRenderer;
+import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 
 @OnlyIn(Dist.CLIENT)
-public class MaggotEntityRender extends LivingRenderer<MaggotEntity, MaggotEntityModel>
+public class MaggotEntityRender extends MobRenderer<MaggotEntity, MaggotEntityModel>
 {
 	public MaggotEntityRender(EntityRendererManager manager) 
 	{
--- src/main/java/landospuk/maggotcheesemod/entities/MaggotEntity.java
+++ src/main/java/landospuk/maggotcheesemod/entities/MaggotEntity.java
@@ -1,21 +1,16 @@
 package landospuk.maggotcheesemod.entities;
 
 import landospuk.maggotcheesemod.init.MaggotEntities;
-import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.goal.BreedGoal;
 import net.minecraft.entity.ai.goal.FleeSunGoal;
 import net.minecraft.entity.ai.goal.LookRandomlyGoal;
-import net.minecraft.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
 import net.minecraft.entity.ai.goal.RandomWalkingGoal;
 import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
 
 public class MaggotEntity extends CreatureEntity{
 
@@ -29,14 +24,12 @@ public class MaggotEntity extends CreatureEntity{
 	protected void registerGoals() 
 	{
 		this.goalSelector.addGoal(0, new SwimGoal(this));
-		this.goalSelector.addGoal(0, new RandomWalkingGoal(this, 0.4d));
-		this.goalSelector.addGoal(0, new LookRandomlyGoal(this));
-//		this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
-		this.goalSelector.addGoal(0, new FleeSunGoal(this, 0.6d));
-		this.goalSelector.addGoal(0, new MeleeAttackGoal(this, 0.4d, true));
-		this.goalSelector.addGoal(0, new WaterAvoidingRandomWalkingGoal(this, interpTargetHeadYaw));
+		this.goalSelector.addGoal(2, new RandomWalkingGoal(this, 0.4d));
+		this.goalSelector.addGoal(3, new LookRandomlyGoal(this));
+//		this.goalSelector.addGoal(1, new BreedGoal(this, 1.0D));
+		this.goalSelector.addGoal(4, new FleeSunGoal(this, 0.6d));
+		this.goalSelector.addGoal(1, new WaterAvoidingRandomWalkingGoal(this, interpTargetHeadYaw));
 	}
-	
 	@Override
 	protected void registerAttributes() 
 	{
@@ -44,9 +37,4 @@ public class MaggotEntity extends CreatureEntity{
 		this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(5.0d);
 		this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.4d);
 	}
-
-//	@Override
-//	public AgeableEntity createChild(AgeableEntity arg0) {
-//		return super.createChild(arg0)
-//	}
 }
